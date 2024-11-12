25cd8450be1b13253491599d0979447657c4dc36
momotadhali <mamotadhali1998@gmail.com>
2024-11-12 12:41:02 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,32 @@
+
+const number = 20;
+const x = 40;
+var y = 30;
+let p = 100;
+
+if (number < 18) {
+  console.log("Good day");
+}
+else if( number<25 ){
+    console.log("mahin");
+}
+if (x < 88) {
+    console.log("good girl");
+  }
+  else if( x<9){
+      console.log("bad girl");
+  }
+  
+  if (y < 55) {
+    console.log("beautiful bangladesh");
+  }
+  else if( y<20){
+      console.log("bad girl");
+  }
+  if (p < 5) {
+    console.log("I Love fuska");
+  }
+  else if( p<200){
+      console.log("I Love My Mother");
+  }
+
---
+++ loop-for.js
@@ -0,0 +1,10 @@
+
+let team = [34,7,2,11,50,49,16,81,100];
+ for(let i = 2; i <=team.length;i++){
+    let element = team[i];
+    console.log(element);
+ }
+
+for(var laptopgiven = 0; laptopgiven < 4; laptopgiven++){
+    console.log("mim","jim","setu");
+console.log(laptopgiven)}
